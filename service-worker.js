3f17e7882dfa01d04e79d9940910c6e5ab5c8b98
AniswagMC <anirudh.duggal.ad.ad@gmail.com>
2023-10-31 14:44:38 -0700

    First public build ready

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,3 @@
-function showImg() {
-  fetch("https://catfact.ninja/fact")
-  .then(res => res.json())
-  .then(fact => console.log(fact.fact))
-}
-
 function bangerMusic() {
   chrome.windows.create({
     focused: true,
@@ -11,8 +5,6 @@ function bangerMusic() {
     url: 'https://www.youtube.com/playlist?list=PLci0CdPZ5bq56I-kUdxuKnBhyAPXv2Asi',
   })
 }
-
-// document.getElementById("hyper-btn").addEventListener("click", showImg)
 
 chrome.action.onClicked.addListener(
   bangerMusic
